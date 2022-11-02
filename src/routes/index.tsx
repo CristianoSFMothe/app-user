00fda2dbb8f0be7fa61efb394a87a49590e53480
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-02 14:04:55 -0300

    :rocket: feat: Adicionado Material UI V5

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@mui/material';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/page-home" element={<p>Página Inicial</p>} />
+      <Route path="/page-home" element={<Button>Test</Button>} />
       
       <Route path="*" element={<Navigate to="/page-home" />} />
     </Routes>
