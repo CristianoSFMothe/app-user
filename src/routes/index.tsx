879ccf6ace9754af9aa6bcdc55e73975d3ecf1b8
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-02 14:21:34 -0300

    :rocket: feat: Adicionando tema Light

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
-import { Button } from '@mui/material';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import {Button} from '@mui/material';
+import {Routes, Route, Navigate} from 'react-router-dom';
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/page-home" element={<Button>Test</Button>} />
-      
-      <Route path="*" element={<Navigate to="/page-home" />} />
+      <Route path="/page-home" element={
+        <Button variant='contained' color='primary'>Test</Button>}/>
+
+      <Route path="*" element={<Navigate to="/page-home"/>}/>
     </Routes>
   );
 };
