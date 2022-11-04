14ce84407ddaf6a48d8d9d9e9fdc7c5e1ca119e3
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-04 11:15:58 -0300

    :rocket: feat: Adicionando o skeleton da barra de carregamento dos botões

--- src/pages/dashboard/Dasboard.tsx
+++ src/pages/dashboard/Dasboard.tsx
@@ -6,7 +6,7 @@ export const Dasboard = () => {
     <LayoutBasePagina
       title='Página inicial'
       toolbar={(
-        <ToolsDetails />
+        <ToolsDetails viewBtnSaveAndClose viewBtnSaveAndCloseLoading />
       )}
     >
       Testando
--- src/shared/components/tools-details/ToolsDetails.tsx
+++ src/shared/components/tools-details/ToolsDetails.tsx
@@ -1,6 +1,48 @@
-import { Box, Button, Divider, Icon, Paper, useTheme } from '@mui/material';
+import { Box, Button, Divider, Icon, Paper, Skeleton, useTheme } from '@mui/material';
 
-export const ToolsDetails: React.FC = () => {
+interface  IToolsDetailsProps {
+  textBtnNew?: string,
+  
+  viewBtnNew?: boolean,
+  viewBtnReturn?: boolean,
+  viewBtnDelete?: boolean,
+  viewBtnSave?: boolean,
+  viewBtnSaveAndClose?: boolean,
+
+  viewBtnNewLoading?: boolean,
+  viewBtnSaveLoading?: boolean,
+  viewBtnReturnLoading?: boolean,
+  viewBtnDeleteLoading?: boolean,
+  viewBtnSaveAndCloseLoading?: boolean,
+
+  clickBtnNew?: () => void,
+  clickBtnReturn?: () => void,
+  clickBtnDelete?: () => void,
+  clickBtnSave?: () => void,
+  clickBtnSaveAndClose?: () => void,
+}
+
+export const ToolsDetails: React.FC<IToolsDetailsProps> = ({
+  textBtnNew = 'Novo',
+  
+  viewBtnNew = true,
+  viewBtnReturn = true,
+  viewBtnDelete = true,
+  viewBtnSave = true,
+  viewBtnSaveAndClose = false,
+                                                             
+  viewBtnNewLoading = false,
+  viewBtnSaveLoading = false,
+  viewBtnReturnLoading = false,
+  viewBtnDeleteLoading = false,
+  viewBtnSaveAndCloseLoading = false,
+
+  clickBtnNew,
+  clickBtnReturn,
+  clickBtnDelete,
+  clickBtnSave,
+  clickBtnSaveAndClose,
+}) => {
   const theme = useTheme();
   
   return(
@@ -15,52 +57,71 @@ export const ToolsDetails: React.FC = () => {
       component={Paper}
       height={theme.spacing(5)}
     >
-      <Button
-        id='btn-saver'
-        color='primary'
-        disableElevation
-        variant='contained'
-        startIcon={<Icon>save</Icon>}
-      >Salvar
-      </Button>
-      
-      <Button
-        id='btn-saver-return'
-        color='primary'
-        disableElevation
-        variant='outlined'
-        startIcon={<Icon>save</Icon>}
-      >Salvar e voltar
-      </Button>
-      
-      <Button
-        id='btn-delete'
-        color='primary'
-        disableElevation
-        variant='outlined'
-        startIcon={<Icon>delete</Icon>}
-      >Apagar
-      </Button> 
-      
-      <Button
-        id='btn-delete'
-        color='primary'
-        disableElevation
-        variant='outlined'
-        startIcon={<Icon>add</Icon>}
-      >Novo
-      </Button>
+      {(viewBtnSave && !viewBtnSaveLoading) &&(
+        <Button
+          id='btn-saver'
+          color='primary'
+          disableElevation
+          variant='contained'
+          onClick={clickBtnSave}
+          startIcon={<Icon>save</Icon>}
+        >Salvar</Button>
+      )}
+
+      {viewBtnSaveLoading && (<Skeleton id='loading-save' width={110} height={60}/>)}
+
+      {(viewBtnSaveAndClose && !viewBtnSaveAndCloseLoading) &&(
+        <Button
+          id='btn-saver-return'
+          color='primary'
+          disableElevation
+          variant='outlined'
+          onClick={clickBtnSaveAndClose}
+          startIcon={<Icon>save</Icon>}
+        >Salvar e voltar</Button>
+      )}
+      {viewBtnSaveAndCloseLoading &&(<Skeleton id='loading-save-close' width={180} height={60}/>)}
+
+      {(viewBtnDelete && !viewBtnDeleteLoading) && (
+        <Button
+          id='btn-delete'
+          color='primary'
+          disableElevation
+          variant='outlined'
+          onClick={clickBtnDelete}
+          startIcon={<Icon>delete</Icon>}
+        >Apagar</Button>
+      )}
+      {viewBtnDeleteLoading &&(<Skeleton id='loading-delete' width={110} height={60}/>)}
+
+      {(viewBtnNew && !viewBtnNewLoading) &&(
+        <Button
+          id='btn-new'
+          color='primary'
+          disableElevation
+          variant='outlined'
+          onClick={clickBtnNew}
+          startIcon={<Icon>add</Icon>}
+        >{textBtnNew}</Button>
+      )}
+      {viewBtnNewLoading &&(<Skeleton id='loading-new' width={110} height={60}/>)}
       
       <Divider variant='middle' orientation='vertical' />
+
+      {(viewBtnReturn && !viewBtnReturnLoading) &&(
+        <Button
+          id='btn-return'
+          color='primary'
+          disableElevation
+          variant='outlined'
+          onClick={clickBtnReturn}
+          startIcon={<Icon>arrow_back</Icon>}
+        >Voltar</Button>
+      )}
       
-      <Button
-        id='btn-return'
-        color='primary'
-        disableElevation
-        variant='outlined'
-        startIcon={<Icon>arrow_back</Icon>}
-      >Voltar
-      </Button>
+      {viewBtnReturnLoading &&(<Skeleton id='loading-return' width={110} height={60}/>)}
+      
     </Box>
+    
   );
 };
